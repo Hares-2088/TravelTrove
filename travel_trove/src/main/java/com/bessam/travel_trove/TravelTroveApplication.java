e6531d3c4878b816ab8d402acfafcc8b68b7b6ac
PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
2024-11-21 21:37:05 -0500

    new springboot app

---
+++ travel_trove/src/main/java/com/bessam/travel_trove/TravelTroveApplication.java
@@ -0,0 +1,13 @@
+package com.bessam.travel_trove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTroveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelTroveApplication.class, args);
+    }
+
+}
