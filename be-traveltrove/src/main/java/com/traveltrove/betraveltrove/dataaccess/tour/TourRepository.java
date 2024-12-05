b343d6690124819a4fc4ddf840ad0b204d5afad3
charlesFinalMS <charles.seg42@gmail.com>
2024-12-05 14:18:27 -0500

    Seperated integration for city and event into the system for adding the data to the separate repositories.

--- be-traveltrove/src/main/java/com/traveltrove/betraveltrove/dataaccess/tour/TourRepository.java
+++ be-traveltrove/src/main/java/com/traveltrove/betraveltrove/dataaccess/tour/TourRepository.java
@@ -1,11 +1,9 @@
 package com.traveltrove.betraveltrove.dataaccess.tour;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface TourRepository extends ReactiveMongoRepository<Tour, String> {
-    public Mono<Tour> findTourByTourId(String tourId);
-    public Flux<Tour> findAllTours();
 
+    public Mono<Tour> findTourByTourId(String tourId);
 }
