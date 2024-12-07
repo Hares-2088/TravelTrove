8cd6755364270772af5ee033456d2162f69e8a45
charlesFinalMS <charles.seg42@gmail.com>
2024-12-07 02:45:26 -0500

    Flux fix on events

--- be-traveltrove/src/main/java/com/traveltrove/betraveltrove/dataaccess/tour/EventRepository.java
+++ be-traveltrove/src/main/java/com/traveltrove/betraveltrove/dataaccess/tour/EventRepository.java
@@ -1,6 +1,7 @@
 package com.traveltrove.betraveltrove.dataaccess.tour;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface EventRepository extends ReactiveMongoRepository<Event, String> {
