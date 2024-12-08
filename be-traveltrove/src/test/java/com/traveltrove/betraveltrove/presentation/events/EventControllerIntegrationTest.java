b19266ccaf4f861788ace4003a1bb7c98ad4f840
PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
2024-12-08 16:25:58 -0500

    updated the filter feature, the add and update event too

---
+++ be-traveltrove/src/test/java/com/traveltrove/betraveltrove/presentation/events/EventControllerIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.traveltrove.betraveltrove.presentation.events;
+
+import com.traveltrove.betraveltrove.dataaccess.events.EventRepository;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"spring.data.mongodb.port=0"})
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class EventControllerIntegrationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    private MockSer
+}
