65b8756e591be31ac25951d50a27c6cab2d59ead
PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
2024-12-11 12:42:40 -0500

    removed an empty file for e2e

--- fe-traveltrove/tests/test-1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/dashboard');
-  await page.getByRole('tab', { name: 'Events' }).click();
-});
