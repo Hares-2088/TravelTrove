7d19324e376339d67d1bfd15d9e4c18eeb324ab2
PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
2025-01-17 16:42:43 -0500

    finished the logic behind the add a new booking :)

--- be-traveltrove/src/main/java/com/traveltrove/betraveltrove/business/booking/BookingServiceImpl.java
+++ be-traveltrove/src/main/java/com/traveltrove/betraveltrove/business/booking/BookingServiceImpl.java
@@ -3,10 +3,15 @@ package com.traveltrove.betraveltrove.business.booking;
 import com.traveltrove.betraveltrove.business.tourpackage.PackageService;
 import com.traveltrove.betraveltrove.business.traveler.TravelerService;
 import com.traveltrove.betraveltrove.business.user.UserService;
+import com.traveltrove.betraveltrove.dataaccess.booking.Booking;
 import com.traveltrove.betraveltrove.dataaccess.booking.BookingRepository;
 import com.traveltrove.betraveltrove.dataaccess.booking.BookingStatus;
 import com.traveltrove.betraveltrove.presentation.booking.BookingRequestModel;
 import com.traveltrove.betraveltrove.presentation.booking.BookingResponseModel;
+import com.traveltrove.betraveltrove.presentation.traveler.TravelerRequestModel;
+import com.traveltrove.betraveltrove.presentation.traveler.TravelerResponseModel;
+import com.traveltrove.betraveltrove.presentation.user.UserResponseModel;
+import com.traveltrove.betraveltrove.presentation.user.UserUpdateRequest;
 import com.traveltrove.betraveltrove.utils.entitymodelyutils.BookingEntityModelUtil;
 import com.traveltrove.betraveltrove.utils.exceptions.InvalidStatusException;
 import com.traveltrove.betraveltrove.utils.exceptions.NotFoundException;
@@ -16,8 +21,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
-import java.util.EnumSet;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -88,27 +95,120 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public Mono<BookingResponseModel> createBooking(BookingRequestModel bookingRequestModel) {
+
         return Mono.just(bookingRequestModel)
                 .map(BookingEntityModelUtil::toBookingEntity)
                 .flatMap(booking ->
+                        // Validate user/package
                         userExistsReactive(booking.getUserId())
                                 .then(packageExistsReactive(booking.getPackageId()))
-                                .then(bookingRepository.findBookingByPackageIdAndUserId(booking.getPackageId(), booking.getUserId())
-                                        .hasElement()
-                                        .flatMap(exists -> {
-                                            if (exists) {
-                                                return Mono.error(new InvalidStatusException("User already has a booking for this package"));
-                                            }
-                                            if (booking.getTravelerIds() != null && !booking.getTravelerIds().isEmpty()) {
-                                                // get the user to access the list of traveler IDs
-                                                userService.syncUserWithAuth0(booking.getUserId())
 
-                                                if ()
-                                            }
-                                            return bookingRepository.save(booking);
-                                        })
-                                )
+                                // Check if booking exists
+                                .then(bookingRepository.findBookingByPackageIdAndUserId(
+                                        booking.getPackageId(), booking.getUserId()
+                                ).hasElement())
+                                .flatMap(exists -> {
+                                    if (exists) {
+                                        return Mono.error(new InvalidStatusException(
+                                                "User already has a booking for this package"));
+                                    }
+                                    return Mono.just(booking);
+                                })
                 )
+
+                // Fetch user from Auth0
+                .flatMap(booking -> userService.syncUserWithAuth0(booking.getUserId())
+                        .switchIfEmpty(Mono.error(
+                                new NotFoundException("User not found: " + booking.getUserId()))
+                        )
+                        .map(activeUser -> Tuples.of(booking, activeUser))
+                )
+
+                // Gather existing traveler details
+                .flatMap(tuple -> {
+                    Booking booking = tuple.getT1();
+                    UserResponseModel activeUser = tuple.getT2();
+
+                    List<String> existingTravelerIds = new ArrayList<>();
+                    if (activeUser.getTravelerIds() != null) {
+                        existingTravelerIds.addAll(activeUser.getTravelerIds());
+                    }
+                    if (activeUser.getTravelerId() != null) {
+                        existingTravelerIds.add(activeUser.getTravelerId());
+                    }
+
+                    return Flux.fromIterable(existingTravelerIds)
+                            .flatMap(travelerId -> travelerService.getTravelerByTravelerId(travelerId))
+                            .collectList()
+                            .map(existingTravelers -> Tuples.of(booking, activeUser, existingTravelers));
+                })
+
+                // Compare travelers & create new ones
+                .flatMap(tuple -> {
+                    Booking booking = tuple.getT1();
+                    UserResponseModel activeUser = tuple.getT2();
+                    List<TravelerResponseModel> existingTravelers = tuple.getT3();
+
+                    return Flux.fromIterable(bookingRequestModel.getTravelers())
+                            .flatMap(requestedTraveler -> {
+                                // does it match an existing traveler?
+                                TravelerResponseModel match = existingTravelers.stream()
+                                        .filter(et -> et.getFirstName().equalsIgnoreCase(requestedTraveler.getFirstName()) &&
+                                                et.getLastName().equalsIgnoreCase(requestedTraveler.getLastName()))
+                                        .findFirst()
+                                        .orElse(null);
+
+                                if (match != null) {
+                                    booking.getTravelerIds().add(match.getTravelerId());
+                                    // Return existing ID
+                                    return Mono.just(match.getTravelerId());
+                                } else {
+                                    // create a new traveler
+                                    TravelerRequestModel newTraveler = TravelerRequestModel.builder()
+                                            .firstName(requestedTraveler.getFirstName())
+                                            .lastName(requestedTraveler.getLastName())
+                                            .addressLine1(requestedTraveler.getAddressLine1())
+                                            .email(requestedTraveler.getEmail())
+                                            .build();
+                                    return travelerService.createTraveler(newTraveler)
+                                            .map(created -> {
+                                                booking.getTravelerIds().add(created.getTravelerId());
+                                                return created.getTravelerId();
+                                            });
+                                }
+                            })
+                            .collectList() // gather all traveler IDs
+                            .map(newlyLinkedIds -> Tuples.of(booking, activeUser, existingTravelers, newlyLinkedIds));
+                })
+
+                // Update the user’s travelerIds set
+                .flatMap(tuple -> {
+                    Booking booking = tuple.getT1();
+                    UserResponseModel activeUser = tuple.getT2();
+                    List<TravelerResponseModel> existingTravelers = tuple.getT3();
+                    List<String> newlyLinkedIds = tuple.getT4();
+
+                    // create a list of already existing traveler IDs and the newly linked ones
+                    Set<String> allTravelerIds = new HashSet<>();
+                    if (activeUser.getTravelerIds() != null) {
+                        allTravelerIds.addAll(activeUser.getTravelerIds());
+                    }
+                    if (activeUser.getTravelerId() != null) {
+                        allTravelerIds.add(activeUser.getTravelerId());
+                    }
+                    allTravelerIds.addAll(newlyLinkedIds);
+
+                    UserUpdateRequest updateRequest = UserUpdateRequest.builder()
+                            .travelerIds(new ArrayList<>(allTravelerIds))
+                            .build();
+
+                    // update user
+                    return userService.updateUserProfile(activeUser.getUserId(), updateRequest)
+                            .thenReturn(booking); // pass booking forward
+                })
+
+                // Save the Booking in Mongo
+                .flatMap(booking -> bookingRepository.save(booking))
                 .map(BookingEntityModelUtil::toBookingResponseModel);
     }
 
--- be-traveltrove/src/test/java/com/traveltrove/betraveltrove/business/booking/BookingServiceUnitTest.java
+++ be-traveltrove/src/test/java/com/traveltrove/betraveltrove/business/booking/BookingServiceUnitTest.java
@@ -1,7 +1,7 @@
 package com.traveltrove.betraveltrove.business.booking;
 
-import com.traveltrove.betraveltrove.business.booking.BookingServiceImpl;
 import com.traveltrove.betraveltrove.business.tourpackage.PackageService;
+import com.traveltrove.betraveltrove.business.traveler.TravelerService;
 import com.traveltrove.betraveltrove.business.user.UserService;
 import com.traveltrove.betraveltrove.dataaccess.booking.Booking;
 import com.traveltrove.betraveltrove.dataaccess.booking.BookingRepository;
@@ -11,6 +11,8 @@ import com.traveltrove.betraveltrove.presentation.booking.BookingRequestModel;
 import com.traveltrove.betraveltrove.presentation.booking.BookingResponseModel;
 import com.traveltrove.betraveltrove.presentation.booking.BookingStatusUpdateRequest;
 import com.traveltrove.betraveltrove.presentation.tourpackage.PackageResponseModel;
+import com.traveltrove.betraveltrove.presentation.traveler.TravelerRequestModel;
+import com.traveltrove.betraveltrove.presentation.traveler.TravelerResponseModel;
 import com.traveltrove.betraveltrove.presentation.user.UserResponseModel;
 import com.traveltrove.betraveltrove.utils.exceptions.NotFoundException;
 import com.traveltrove.betraveltrove.utils.exceptions.SameStatusException;
@@ -24,9 +26,11 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.time.LocalDate;
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,16 +48,21 @@ public class BookingServiceUnitTest {
     @Mock
     private PackageService packageService;
 
+    @Mock
+    private TravelerService travelerService;
+
     private final String bookingId1 = "a0fca78b-54e6-4428-8ec3-8a42ac0dfd4e";
     private final String bookingId2 = "b1fdf006-b0c1-49fe-98ed-21f816ad524e";
 
     private final String packageId1 = "2484e7bf-51ee-445f-86de-cb5c1a349954";
     private final String packageId2 = "8c4a4b10-aed4-4fa1-8268-2c372df4b072";
 
+    private final List<String> travelerIds = List.of("1", "2", "3");
+
     @Test
     public void whenGetAllBookings_thenReturnAllBookings() {
-        Booking booking1 = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.BOOKING_CONFIRMED, LocalDate.of(2025, 4, 4));
-        Booking booking2 = new Booking("2", bookingId2, "user2", "pack2", 1300.00, BookingStatus.PAYMENT_PENDING, LocalDate.of(2025, 5, 5));
+        Booking booking1 = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.BOOKING_CONFIRMED, LocalDate.of(2025, 4, 4), travelerIds);
+        Booking booking2 = new Booking("2", bookingId2, "user2", "pack2", 1300.00, BookingStatus.PAYMENT_PENDING, LocalDate.of(2025, 5, 5), travelerIds);
 
         when(bookingRepository.findAll()).thenReturn(Flux.just(booking1, booking2));
 
@@ -76,7 +85,8 @@ public class BookingServiceUnitTest {
                 existingPackageId,
                 1200.00,
                 BookingStatus.BOOKING_CONFIRMED,
-                LocalDate.of(2025, 4, 4)
+                LocalDate.of(2025, 4, 4),
+                java.util.List.of("1", "2", "3")
         );
 
         Booking booking2 = new Booking(
@@ -86,7 +96,8 @@ public class BookingServiceUnitTest {
                 existingPackageId,
                 1300.00,
                 BookingStatus.PAYMENT_PENDING,
-                LocalDate.of(2025, 5, 5)
+                LocalDate.of(2025, 5, 5),
+                java.util.List.of("4", "5", "6")
         );
 
         // Mock PackageService to return a non-empty Mono (valid package), simulating that the package exists
@@ -136,7 +147,7 @@ public class BookingServiceUnitTest {
 
     @Test
     public void whenGetBooking_withExistingBookingId_thenReturnBooking() {
-        Booking booking = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.BOOKING_CONFIRMED, LocalDate.of(2025, 4, 4));
+        Booking booking = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.BOOKING_CONFIRMED, LocalDate.of(2025, 4, 4), travelerIds);
 
         when(bookingRepository.findBookingByBookingId(bookingId1)).thenReturn(Mono.just(booking));
 
@@ -156,55 +167,131 @@ public class BookingServiceUnitTest {
     }
 
     @Test
-    public void whenCreateBooking_thenReturnSavedBooking() {
-        String userId = "user1";
-        String packageId = "pack1";
+    public void whenCreateBooking_withTravelers_thenCreateOrMergeTravelersAndSaveBooking() {
+        // -------------------------------------------------------
+        // 1) Define INPUT: A BookingRequestModel with travelers
+        // -------------------------------------------------------
+        String userId = "user123";
+        String packageId = "packABC";
+
+        // Suppose the user wants to book with two travelers: one that already exists, one new
+        List<TravelerRequestModel> requestedTravelers = List.of(
+                TravelerRequestModel.builder()
+                        .firstName("Alice")
+                        .lastName("Wonderland")
+                        .build(),
+                TravelerRequestModel.builder()
+                        .firstName("Bob")
+                        .lastName("Marley")
+                        .build()
+        );
 
         BookingRequestModel requestModel = BookingRequestModel.builder()
-                .userId(userId) // Ensure userId is set
+                .userId(userId)
                 .packageId(packageId)
                 .totalPrice(1200.00)
                 .status(BookingStatus.PAYMENT_PENDING)
                 .bookingDate(LocalDate.of(2025, 4, 4))
+                .travelers(requestedTravelers)  // <--- The travelers
                 .build();
 
-        Booking booking = new Booking("1", "fcd8ae00-4251-4b38-bafe-a48e131cb5d4", userId, packageId, 1200.00, BookingStatus.PAYMENT_PENDING, LocalDate.of(2025, 4, 4));
+        // -------------------------------------------------------
+        // 2) Define what the final saved Booking should look like
+        // (after traveler logic). We'll mock the "save" result.
+        // -------------------------------------------------------
+        List<String> finalTravelerIds = List.of("TID-EXISTING-111", "TID-NEW-222");
 
-        // Mock userService
-        when(userService.syncUserWithAuth0(userId))
-                .thenReturn(Mono.just(UserResponseModel.builder()
-                        .userId(userId)
-                        .build()));
+        Booking savedBooking = Booking.builder()
+                .id("1")
+                .bookingId("generated-booking-id-123")
+                .userId(userId)
+                .packageId(packageId)
+                .totalPrice(1200.00)
+                .status(BookingStatus.PAYMENT_PENDING)
+                .bookingDate(LocalDate.of(2025, 4, 4))
+                .travelerIds(finalTravelerIds)
+                .build();
 
-        // Mock packageService
+        // -------------------------------------------------------
+        // 3) MOCK DEPENDENCIES
+        // -------------------------------------------------------
+
+        // 3a) PackageService => The package must exist
         when(packageService.getPackageByPackageId(packageId))
-                .thenReturn(Mono.just(PackageResponseModel.builder()
-                        .packageId(packageId)
-                        .build()));
+                .thenReturn(Mono.just(
+                        PackageResponseModel.builder()
+                                .packageId(packageId)
+                                .build()
+                ));
 
-        // Mock bookingRepository
+        // 3b) userService.syncUserWithAuth0(...)
+        UserResponseModel mockUser = UserResponseModel.builder()
+                .userId(userId)
+                .travelerId("TID-EXISTING-111")   // One "primary" traveler ID
+                .travelerIds(new ArrayList<>())   // For illustration, empty list
+                .build();
+        when(userService.syncUserWithAuth0(userId))
+                .thenReturn(Mono.just(mockUser));
+
+        // 3c) travelerService.getTravelerByTravelerId(...)
+        // We only expect "TID-EXISTING-111" to be fetched, so let's remove the fallback.
+        TravelerResponseModel existingTraveler = TravelerResponseModel.builder()
+                .travelerId("TID-EXISTING-111")
+                .firstName("Alice")
+                .lastName("Wonderland")
+                .build();
+        when(travelerService.getTravelerByTravelerId("TID-EXISTING-111"))
+                .thenReturn(Mono.just(existingTraveler));
+
+        // 3d) travelerService.createTraveler(...) => for the brand-new traveler (Bob Marley)
+        TravelerResponseModel newTraveler = TravelerResponseModel.builder()
+                .travelerId("TID-NEW-222")
+                .firstName("Bob")
+                .lastName("Marley")
+                .build();
+        when(travelerService.createTraveler(any(TravelerRequestModel.class)))
+                .thenReturn(Mono.just(newTraveler));
+
+        // 3e) userService.updateUserProfile(...)
+        when(userService.updateUserProfile(eq(userId), any()))
+                .thenReturn(Mono.just(
+                        mockUser.toBuilder()
+                                // now user has both TID-EXISTING-111 & TID-NEW-222
+                                .travelerIds(List.of("TID-EXISTING-111", "TID-NEW-222"))
+                                .build()
+                ));
+
+        // 3f) bookingRepository => must not already exist
         when(bookingRepository.findBookingByPackageIdAndUserId(packageId, userId))
                 .thenReturn(Mono.empty());
-        when(bookingRepository.save(any(Booking.class)))
-                .thenReturn(Mono.just(booking));
 
-        // Verify
+        // 3g) bookingRepository.save(...) => return the final saved booking
+        when(bookingRepository.save(any(Booking.class)))
+                .thenReturn(Mono.just(savedBooking));
+
+        // -------------------------------------------------------
+        // 4) VERIFY
+        // -------------------------------------------------------
         StepVerifier.create(bookingService.createBooking(requestModel))
-                .expectNextMatches(response ->
-                        response.getUserId().equals(userId) &&
-                                response.getPackageId().equals(packageId) &&
-                                response.getTotalPrice() == 1200.00 &&
-                                response.getStatus() == BookingStatus.PAYMENT_PENDING &&
-                                response.getBookingDate().equals(LocalDate.of(2025, 4, 4))
-                )
+                .assertNext(response -> {
+                    assertNotNull(response);
+                    assertEquals("generated-booking-id-123", response.getBookingId());
+                    assertEquals(2, response.getTravelerIds().size());
+                    assertTrue(response.getTravelerIds().contains("TID-EXISTING-111"));
+                    assertTrue(response.getTravelerIds().contains("TID-NEW-222"));
+                    assertEquals(BookingStatus.PAYMENT_PENDING, response.getStatus());
+                })
                 .verifyComplete();
     }
 
+
     @Test
     public void whenUpdateBookingStatus_withExistingId_thenReturnUpdatedBooking() {
-        Booking booking = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.PAYMENT_PENDING, LocalDate.of(2025, 4, 4));
-        Booking updatedBooking = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.BOOKING_CONFIRMED, LocalDate.of(2025, 4, 4));
 
+    List<String> travelerIds = List.of("1", "2", "3");
+
+    Booking booking = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.PAYMENT_PENDING, LocalDate.of(2025, 4, 4), travelerIds);
+    Booking updatedBooking = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.BOOKING_CONFIRMED, LocalDate.of(2025, 4, 4), travelerIds);
         // Mock repository calls
         when(bookingRepository.findBookingByBookingId(bookingId1)).thenReturn(Mono.just(booking));
         when(bookingRepository.save(booking)).thenReturn(Mono.just(updatedBooking));
@@ -238,7 +325,7 @@ public class BookingServiceUnitTest {
 
     @Test
     public void whenUpdateBookingStatus_withSameStatus_thenReturnSameStatusException() {
-        Booking booking = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.BOOKING_CONFIRMED, LocalDate.of(2025, 4, 4));
+        Booking booking = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.BOOKING_CONFIRMED, LocalDate.of(2025, 4, 4), travelerIds);
 
         // Mock repository to return a booking with the same status
         when(bookingRepository.findBookingByBookingId(bookingId1)).thenReturn(Mono.just(booking));
@@ -256,7 +343,7 @@ public class BookingServiceUnitTest {
 
     @Test
     public void whenDeleteBooking_withExistingId_thenCompleteSuccessfully() {
-        Booking booking = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.BOOKING_CONFIRMED, LocalDate.of(2025, 4, 4));
+        Booking booking = new Booking("1", bookingId1, "user1", "pack1", 1200.00, BookingStatus.BOOKING_CONFIRMED, LocalDate.of(2025, 4, 4), travelerIds);
 
         when(bookingRepository.findBookingByBookingId(bookingId1)).thenReturn(Mono.just(booking));
         when(bookingRepository.delete(booking)).thenReturn(Mono.empty());
