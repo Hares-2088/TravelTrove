40f94294d0ec6c3f22e7004d9d82ab755fc5f2d9
PortgasD.Adem <121312399+Hares-2088@users.noreply.github.com>
2025-01-19 23:21:29 -0500

    fix

--- fe-traveltrove/src/router.tsx
+++ fe-traveltrove/src/router.tsx
@@ -1,19 +1,19 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
-import { AppRoutes } from './shared/models/app.routes';
-import ProtectedRoute from './shared/components/ProtectedRoute';
-import DashboardPage from './pages/staff/DashboardPage';
-import ToursPage from './pages/tours/ToursPage';
-import TourDetailsPage from './pages/tours/TourDetailsPage';
-import HomePage from './pages/home/HomePage';
-import Layout from './layouts/Layout';
-import CallbackPage from './pages/CallbackPage';
-import UnauthorizedPage from './pages/errors/UnauthorizedPage';
-import NotFoundPage from './pages/errors/NotFoundPage';
-import ServiceUnavailablePage from './pages/errors/ServiceNotAvailablePage';
-import InternalServerErrorPage from './pages/errors/InternalServerErrorPage';
-import RequestTimeoutPage from './pages/errors/RequestTimeoutPage';
-import ForbiddenPage from './pages/errors/ForbiddenPage';
-import Bookings from './features/staff/components/Pages/Bookings';
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import { AppRoutes } from "./shared/models/app.routes";
+import ProtectedRoute from "./shared/components/ProtectedRoute";
+import DashboardPage from "./pages/staff/DashboardPage";
+import ToursPage from "./pages/tours/ToursPage";
+import TourDetailsPage from "./pages/tours/TourDetailsPage";
+import HomePage from "./pages/home/HomePage";
+import Layout from "./layouts/Layout";
+import CallbackPage from "./pages/CallbackPage";
+import UnauthorizedPage from "./pages/errors/UnauthorizedPage";
+import NotFoundPage from "./pages/errors/NotFoundPage";
+import ServiceUnavailablePage from "./pages/errors/ServiceNotAvailablePage";
+import InternalServerErrorPage from "./pages/errors/InternalServerErrorPage";
+import RequestTimeoutPage from "./pages/errors/RequestTimeoutPage";
+import ForbiddenPage from "./pages/errors/ForbiddenPage";
+import Bookings from "./features/staff/components/Pages/Bookings";
 import ProfileCreatePage from "./pages/user/ProfileCreatePage.tsx"; // Import the Bookings component
 import PackageDetailsPage from "./pages/packages/PackageDetailsPage";
 import BookingFormPage from "./pages/booking/BookingFormPage";
@@ -51,19 +51,11 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: AppRoutes.Bookings, 
+        path: AppRoutes.Bookings,
         element: (
           <ProtectedRoute>
             <Bookings />
           </ProtectedRoute>
-        ),
-      },
-      {
-        path: AppRoutes.ProfileCreatePage,
-        element: (
-            <ProtectedRoute>
-              <ProfileCreatePage />
-            </ProtectedRoute>
         ),
       },
       {
@@ -79,6 +71,14 @@ const router = createBrowserRouter([
         element: (
           <ProtectedRoute>
             <BookingFormPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: AppRoutes.ProfileCreatePage,
+        element: (
+          <ProtectedRoute>
+            <ProfileCreatePage />
           </ProtectedRoute>
         ),
       },
@@ -122,6 +122,7 @@ const router = createBrowserRouter([
           </ProtectedRoute>
         ),
       },
+
       {
         path: AppRoutes.Default,
         element: <Navigate to={AppRoutes.Home} replace />,
